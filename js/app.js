4145c0f87c6bf46d3f47963ae0831d75f82f20ab
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-01-19 13:24:32 +0000

    Rename app to tinata

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-// Ionic Starter App, v0.9.20
+// Ionic tinata App, v0.9.20
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// 'tinata' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.services', 'starter.controllers'])
+// 'tinata.services' is found in services.js
+// 'tinata.controllers' is found in controllers.js
+angular.module('tinata', ['ionic', 'tinata.services', 'tinata.controllers'])
 
 
 .config(function($stateProvider, $urlRouterProvider) {
